b5d0885e8aab48fd78e5ed2373b3564681f264f0
Eisiehame <knurov24@gmail.com>
2023-06-06 22:07:36 +0200

    bad products added

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,8 +1,39 @@
 from django import forms
-from catalog.models import Product
+from catalog.models import Product, Version
 
-class ProductForm(forms.ModelForm):
+bad_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
+class FormStyleMixin:
+    def __int__(self, *args, **kwargs):
+        super().__int__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+class ProductForm(FormStyleMixin, forms.ModelForm):
+
     class Meta:
         model = Product
         fields = "__all__"
         #fields = ('name', 'description', 'preview_image', 'category', 'price')
+
+    def clean_name(self):
+        cleaned_data = self.cleaned_data['name']
+        if cleaned_data in bad_words:
+            raise forms.ValidationError('Эти продукты запрещены')
+
+        return cleaned_data
+
+    def clean_description(self):
+        cleaned_data = self.cleaned_data['description']
+        if cleaned_data in bad_words:
+            raise forms.ValidationError('Эти продукты запрещены')
+
+        return cleaned_data
+
+class VersionForm(forms.ModelForm):
+
+    class Meta:
+        model = Version
+        fields = "__all__"
+
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+from django.forms import inlineformset_factory
 
-from catalog.forms import ProductForm
-from catalog.models import Product, Blog
+from catalog.forms import ProductForm, VersionForm
+from catalog.models import Product, Blog, Version
 from django.views import generic
 from django.urls import reverse_lazy
 
@@ -45,10 +46,28 @@ class ProductDeleteView(generic.DeleteView):
 class ProductUpdateView(generic.UpdateView):
     model = Product
     form_class = ProductForm
-    template_name = 'catalog/product_form_with_formset.html'
+    template_name = 'catalog/product_form.html'
     # fields = ('name', 'description', 'preview_image', 'category', 'price')
-    success_url = reverse_lazy('catalog:blog_list')
+    success_url = reverse_lazy('catalog:product_list')
 
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        VersionFormset = inlineformset_factory(Product, Version, form=VersionForm, extra=1)
+        if self.request.method == 'POST':
+            context_data['formset'] = VersionFormset(self.request.POST, instance=self.object)
+        else:
+            context_data['formset'] = VersionFormset(instance=self.object)
+        return context_data
+
+    def form_valid(self, form):
+        context_data = self.get_context_data()
+        formset = context_data['formset']
+        self.object = form.save()
+        if formset.is_valid():
+            formset.instance = self.object
+            formset.save()
+
+        return super().form_valid(form)
 
 class BlogListView(generic.ListView):
     model = Blog
