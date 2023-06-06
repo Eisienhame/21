7d711aa432631af0ac25fe34af327167d86287e5
Eisiehame <knurov24@gmail.com>
2023-06-06 19:54:16 +0200

    add version 1

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from catalog.models import Category, Product, Blog
+from catalog.models import Category, Product, Blog, Version
 # Register your models here.
 # admin.site.register(Category)
 # admin.site.register(Product)
@@ -18,3 +18,9 @@ class CategoryAdmin(admin.ModelAdmin):
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('article_title', 'active_of_publication')
     prepopulated_fields = {"slug": ("article_title",)}
+
+@admin.register(Version)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'number_ver', 'name_ver')
+    list_filter = ('name_ver',)
+
