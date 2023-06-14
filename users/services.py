8d4f42c98ca37be34de9f734f96f2c487c454b01
Eisiehame <knurov24@gmail.com>
2023-06-14 22:27:38 +0200

    fix4 confirm_user

--- users/services.py
+++ users/services.py
@@ -5,7 +5,7 @@ from django.core.mail import send_mail
 def confirm_account(obj):
     send_mail(
         subject='Подтверждение почты',
-        message=f'Для подтверждения регистрации перейдите по ссылке: http://localhost:8000/users/activate/{obj.token}/',
+        message=f'Для подтверждения регистрации перейдите по ссылке: http://localhost:8000/users/activate/{obj.email}/',
         from_email=settings.EMAIL_HOST_USER,
         recipient_list=[obj.email],
         fail_silently=False
--- users/views.py
+++ users/views.py
@@ -2,6 +2,8 @@ from django.views.generic import UpdateView, CreateView
 from users.models import User
 from django.urls import reverse_lazy
 from users.forms import UserForm, UserRegisterForm
+from users.services import confirm_account
+from django.shortcuts import redirect, render
 
 
 class ProfileUpdateView(UpdateView):
@@ -20,3 +22,20 @@ class RegisterView(CreateView):
     form_class = UserRegisterForm
     success_url = reverse_lazy('catalog:homepage')
 
+    def form_valid(self, form):
+        self.object = form.save()
+        self.object.is_active = False
+        confirm_account(self.object)
+        self.object.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        url_1 = super().get_success_url()
+        return str(url_1) + str(self.object.email)
+
+
+def activate_user(request, email):
+    user = User.objects.filter(email=email)
+    user.is_active = True
+    user.save()
+    return redirect('users:login')
